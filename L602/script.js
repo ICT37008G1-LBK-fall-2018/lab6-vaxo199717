e9f5fe65cfb932cd9b7ab944ac553ab935f84c55
vaxo ozgebishvili <vaxoozgebishvili.27@gmail.com>
2018-12-01 09:14:33 -0800

    L602

---
+++ L602/script.js
@@ -0,0 +1,6 @@
+let firstElem = document.querySelector("#first"),
+  secondElem = document.querySelector("#second");
+function insertAfter(newNode, currNode) {
+  currNode.parentNode.insertBefore(newNode, currNode.nextSibling);
+}
+insertAfter(firstElem, secondElem);
